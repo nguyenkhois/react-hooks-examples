aa831b536705ecd7778376e128e6eb0d106bbdff
Khoi Le <minarek@outlook.com>
2019-03-05 09:51:23 +0100

    Initial commit for the simple React build environment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Hello from './Hello';
+
+class App extends Component {
+    //Do stuff
+    
+    //Render
+    render(){
+        return(
+            <Hello/>
+        );
+    }
+};
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    //Do stuff
+    
+    //Render
+    render(){
+        return(
+            <div className="welcome">
+                Welcome to React and Pure JavaScript! Babel 7 :-)
+            </div>
+        );
+    }
+};
+
+export default Hello;
